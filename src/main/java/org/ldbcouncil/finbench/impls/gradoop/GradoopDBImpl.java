3912b70f55e198f82f4ab4e5a29d402794457052
cubexy <54584804+cubexy@users.noreply.github.com>
2024-08-21 22:18:01 +0200

    Changed gradoop import

--- src/main/java/org/ldbcouncil/finbench/impls/gradoop/GradoopDBImpl.java
+++ src/main/java/org/ldbcouncil/finbench/impls/gradoop/GradoopDBImpl.java
@@ -7,17 +7,19 @@ import org.ldbcouncil.finbench.driver.DbConnectionState;
 import org.ldbcouncil.finbench.driver.DbException;
 import org.ldbcouncil.finbench.driver.log.LoggingService;
 import org.apache.flink.api.java.ExecutionEnvironment;
-import org.gradoop.temporal.util.TemporalGradoopConfig;
+import org.gradoop.temporal.util.*;
 
 import java.io.IOException;
 import java.util.Map;
 
 public class GradoopDBImpl extends Db {
     static Logger logger = LogManager.getLogger("GradoopDbImpl");
+    private ExecutionEnvironment env;
 
     @Override
     protected void onInit(Map<String, String> properties, LoggingService loggingService) throws DbException {
         logger.info("Initializing Gradoop DB");
+        this.env = ExecutionEnvironment.getExecutionEnvironment();
         //complex reads go here
 
         //simple reads go here
