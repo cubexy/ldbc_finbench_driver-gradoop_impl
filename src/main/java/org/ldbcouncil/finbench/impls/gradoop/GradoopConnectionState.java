25143f0cb6ec229496c27b7ad96124710cff5d5a
cubexy <54584804+cubexy@users.noreply.github.com>
2024-08-22 20:34:16 +0200

    Added empty connection state class

---
+++ src/main/java/org/ldbcouncil/finbench/impls/gradoop/GradoopConnectionState.java
@@ -0,0 +1,16 @@
+package org.ldbcouncil.finbench.impls.gradoop;
+
+import org.ldbcouncil.finbench.driver.DbConnectionState;
+
+import java.io.IOException;
+
+public class GradoopConnectionState extends DbConnectionState {
+
+    public GradoopConnectionState() {
+    }
+
+    @Override
+    public void close() throws IOException {
+    }
+
+}
