8550f08d3f82c7e1d2bc3d96bc458961fb378452
cubexy <54584804+cubexy@users.noreply.github.com>
2024-08-29 14:41:51 +0200

    Finalized all handler boilerplate code

--- src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/read/complex/ComplexRead6Handler.java
+++ src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/read/complex/ComplexRead6Handler.java
@@ -1,4 +1,63 @@
 package org.ldbcouncil.finbench.impls.gradoop.queries.read.complex;
 
-public class ComplexRead6Handler {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.tuple.Tuple1;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.gradoop.flink.model.api.operators.UnaryBaseGraphToValueOperator;
+import org.gradoop.temporal.model.impl.TemporalGraph;
+import org.ldbcouncil.finbench.driver.DbException;
+import org.ldbcouncil.finbench.driver.OperationHandler;
+import org.ldbcouncil.finbench.driver.ResultReporter;
+import org.ldbcouncil.finbench.driver.truncation.TruncationOrder;
+import org.ldbcouncil.finbench.driver.workloads.transaction.queries.ComplexRead6;
+import org.ldbcouncil.finbench.driver.workloads.transaction.queries.ComplexRead6Result;
+import org.ldbcouncil.finbench.driver.workloads.transaction.queries.ComplexRead6;
+import org.ldbcouncil.finbench.impls.gradoop.GradoopFinbenchBaseGraphState;
+import org.ldbcouncil.finbench.impls.gradoop.GradoopImpl;
+
+public class ComplexRead6Handler implements OperationHandler<ComplexRead6, GradoopFinbenchBaseGraphState> {
+
+    @Override
+    public void executeOperation(ComplexRead6 cr6, GradoopFinbenchBaseGraphState connectionState,
+                                 ResultReporter resultReporter) throws DbException {
+        GradoopImpl.logger.info(cr6.toString());
+        DataSet<Tuple3<Long, Double, Double>> complexRead6Result = new ComplexRead6GradoopOperator(cr6).execute(connectionState.getGraph());
+        List<ComplexRead6Result> complexRead6Results = new ArrayList<>();
+        try {
+            complexRead6Result.collect().forEach(
+                tuple -> complexRead6Results.add(new ComplexRead6Result(tuple.f0, tuple.f1, tuple.f2)));
+        } catch (Exception e) {
+            throw new DbException("Error while collecting results for complex read 6: " + e);
+        }
+        resultReporter.report(complexRead6Results.size(), complexRead6Results, cr6);
+    }
+}
+
+class ComplexRead6GradoopOperator implements UnaryBaseGraphToValueOperator<TemporalGraph, DataSet<Tuple3<Long, Double, Double>>> {
+
+    private final Long id;
+    private final Date startTime;
+    private final Date endTime;
+    private final Integer truncationLimit;
+    private final TruncationOrder truncationOrder;
+    private final Double threshold1;
+    private final Double threshold2;
+
+    public ComplexRead6GradoopOperator(ComplexRead6 cr6) {
+        this.id = cr6.getId();
+        this.startTime = cr6.getStartTime();
+        this.endTime = cr6.getEndTime();
+        this.truncationLimit = cr6.getTruncationLimit();
+        this.truncationOrder = cr6.getTruncationOrder();
+        this.threshold1 = cr6.getThreshold1();
+        this.threshold2 = cr6.getThreshold2();
+    }
+
+    @Override
+    public DataSet<Tuple3<Long, Double, Double>> execute(TemporalGraph temporalGraph) {
+        return null;
+    }
 }
--- src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/read/complex/ComplexRead9Handler.java
+++ src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/read/complex/ComplexRead9Handler.java
@@ -1,4 +1,61 @@
 package org.ldbcouncil.finbench.impls.gradoop.queries.read.complex;
 
-public class ComplexRead9Handler {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.tuple.Tuple1;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.gradoop.flink.model.api.operators.UnaryBaseGraphToValueOperator;
+import org.gradoop.temporal.model.impl.TemporalGraph;
+import org.ldbcouncil.finbench.driver.DbException;
+import org.ldbcouncil.finbench.driver.OperationHandler;
+import org.ldbcouncil.finbench.driver.ResultReporter;
+import org.ldbcouncil.finbench.driver.truncation.TruncationOrder;
+import org.ldbcouncil.finbench.driver.workloads.transaction.queries.ComplexRead9;
+import org.ldbcouncil.finbench.driver.workloads.transaction.queries.ComplexRead9Result;
+import org.ldbcouncil.finbench.driver.workloads.transaction.queries.ComplexRead9;
+import org.ldbcouncil.finbench.impls.gradoop.GradoopFinbenchBaseGraphState;
+import org.ldbcouncil.finbench.impls.gradoop.GradoopImpl;
+
+public class ComplexRead9Handler implements OperationHandler<ComplexRead9, GradoopFinbenchBaseGraphState> {
+
+    @Override
+    public void executeOperation(ComplexRead9 cr9, GradoopFinbenchBaseGraphState connectionState,
+                                 ResultReporter resultReporter) throws DbException {
+        GradoopImpl.logger.info(cr9.toString());
+        DataSet<Tuple3<Float, Float, Float>> complexRead9Result = new ComplexRead9GradoopOperator(cr9).execute(connectionState.getGraph());
+        List<ComplexRead9Result> complexRead9Results = new ArrayList<>();
+        try {
+            complexRead9Result.collect().forEach(
+                tuple -> complexRead9Results.add(new ComplexRead9Result(tuple.f0, tuple.f1, tuple.f2)));
+        } catch (Exception e) {
+            throw new DbException("Error while collecting results for complex read 9: " + e);
+        }
+        resultReporter.report(complexRead9Results.size(), complexRead9Results, cr9);
+    }
+}
+
+class ComplexRead9GradoopOperator implements UnaryBaseGraphToValueOperator<TemporalGraph, DataSet<Tuple3<Float, Float, Float>>> {
+
+    private final Long id;
+    private final Date startTime;
+    private final Date endTime;
+    private final Integer truncationLimit;
+    private final TruncationOrder truncationOrder;
+    private final Double threshold;
+
+    public ComplexRead9GradoopOperator(ComplexRead9 cr9) {
+        this.id = cr9.getId();
+        this.startTime = cr9.getStartTime();
+        this.endTime = cr9.getEndTime();
+        this.truncationLimit = cr9.getTruncationLimit();
+        this.truncationOrder = cr9.getTruncationOrder();
+        this.threshold = cr9.getThreshold();
+    }
+
+    @Override
+    public DataSet<Tuple3<Float, Float, Float>> execute(TemporalGraph temporalGraph) {
+        return null;
+    }
 }
