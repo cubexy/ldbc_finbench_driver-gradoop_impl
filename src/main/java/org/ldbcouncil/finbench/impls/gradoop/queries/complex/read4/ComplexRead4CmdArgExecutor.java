51f61609ebe73aec8986432d3f2d1034cc649486
cubexy <54584804+cubexy@users.noreply.github.com>
2024-12-19 21:47:42 +0100

    init cr4

---
+++ src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/complex/read4/ComplexRead4CmdArgExecutor.java
@@ -0,0 +1,35 @@
+package org.ldbcouncil.finbench.impls.gradoop.queries.complex.read4;
+
+import java.util.List;
+import org.ldbcouncil.finbench.driver.DbException;
+import org.ldbcouncil.finbench.driver.workloads.transaction.queries.ComplexRead4;
+import org.ldbcouncil.finbench.driver.workloads.transaction.queries.ComplexRead4Result;
+import org.ldbcouncil.finbench.impls.gradoop.FlinkCmdArg;
+import org.ldbcouncil.finbench.impls.gradoop.GradoopFinbenchBaseGraphState;
+import org.ldbcouncil.finbench.impls.gradoop.queries.AbstractCmdArgExecutor;
+
+public class ComplexRead4CmdArgExecutor extends AbstractCmdArgExecutor<List<ComplexRead4Result>> {
+
+    private ComplexRead4GradoopOperator operator;
+
+    @Override
+    public String getQueryTitle() {
+        return "ComplexRead4";
+    }
+
+    @Override
+    public String getQueryKey() {
+        return "complex_read_4";
+    }
+
+    @Override
+    public List<ComplexRead4Result> executeQuery(GradoopFinbenchBaseGraphState graph) throws DbException {
+        return operator.execute(graph.getGraph());
+    }
+
+    @Override
+    public void init(FlinkCmdArg inputArgs) {
+        ComplexRead4 input = new ComplexRead4(inputArgs.getId(), inputArgs.getId2(), inputArgs.getStartTime(), inputArgs.getEndTime());
+        this.operator = new ComplexRead4GradoopOperator(input);
+    }
+}
--- src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/complex/read4/ComplexRead4Handler.java
+++ src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/complex/read4/ComplexRead4Handler.java
@@ -18,14 +18,7 @@ public class ComplexRead4Handler implements OperationHandler<ComplexRead4, Grado
     public void executeOperation(ComplexRead4 cr4, GradoopFinbenchBaseGraphState connectionState,
                                  ResultReporter resultReporter) throws DbException {
         GradoopImpl.logger.info(cr4.toString());
-        DataSet<Tuple7<Long, Long, Double, Double, Long, Double, Double>> complexRead4Result = new ComplexRead4GradoopOperator(cr4).execute(connectionState.getGraph());
-        List<ComplexRead4Result> complexRead4Results = new ArrayList<>();
-        try {
-            complexRead4Result.collect().forEach(
-                tuple -> complexRead4Results.add(new ComplexRead4Result(tuple.f0, tuple.f1, tuple.f2, tuple.f3, tuple.f4, tuple.f5, tuple.f6)));
-        } catch (Exception e) {
-            throw new DbException("Error while collecting results for complex read 4: " + e);
-        }
+        List<ComplexRead4Result> complexRead4Results = new ComplexRead4GradoopOperator(cr4).execute(connectionState.getGraph());
         resultReporter.report(complexRead4Results.size(), complexRead4Results, cr4);
     }
 }
