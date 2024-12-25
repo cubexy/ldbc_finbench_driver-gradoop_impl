97a31560f707986c2872a04c7fe802c6dc74165d
cubexy <54584804+cubexy@users.noreply.github.com>
2024-12-25 15:07:48 +0100

    WIP cr4

--- src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/complex/read4/ComplexRead4GradoopOperator.java
+++ src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/complex/read4/ComplexRead4GradoopOperator.java
@@ -1,11 +1,16 @@
 package org.ldbcouncil.finbench.impls.gradoop.queries.complex.read4;
 
-import java.util.Date;
+import java.util.Arrays;
 import java.util.List;
-import org.apache.flink.api.java.DataSet;
-import org.apache.flink.api.java.tuple.Tuple7;
 import org.gradoop.flink.model.api.operators.UnaryBaseGraphToValueOperator;
+import org.gradoop.flink.model.impl.functions.epgm.LabelIsIn;
+import org.gradoop.flink.model.impl.operators.aggregation.functions.count.Count;
+import org.gradoop.flink.model.impl.operators.aggregation.functions.sum.SumProperty;
+import org.gradoop.flink.model.impl.operators.combination.ReduceCombination;
+import org.gradoop.flink.model.impl.operators.keyedgrouping.GroupingKeys;
+import org.gradoop.flink.model.impl.operators.keyedgrouping.KeyedGrouping;
 import org.gradoop.temporal.model.impl.TemporalGraph;
+import org.gradoop.temporal.model.impl.TemporalGraphCollection;
 import org.ldbcouncil.finbench.driver.workloads.transaction.queries.ComplexRead4;
 import org.ldbcouncil.finbench.driver.workloads.transaction.queries.ComplexRead4Result;
 
@@ -26,6 +31,23 @@ class ComplexRead4GradoopOperator implements
 
     @Override
     public List<ComplexRead4Result> execute(TemporalGraph temporalGraph) {
+        TemporalGraph windowedGraph = temporalGraph
+            .subgraph(new LabelIsIn<>("Account"), new LabelIsIn<>("transfer"))
+            .fromTo(this.startTime, this.endTime);
+
+        windowedGraph.query(
+            "MATCH (src:Account)-[edge1:transfer]->(dst:Account), (src)<-[edge2:transfer]-(other:Account)-[edge3:transfer]->(dst) WHERE src.id = " +
+                this.id1 + "L AND dst.id = " + this.id2 + "L")
+            .reduce(new ReduceCombination<>())
+            .callForGraph(
+                new KeyedGrouping<>(Arrays.asList(GroupingKeys.label(), GroupingKeys.property("id")),
+                    null, null,
+                    Arrays.asList(new Count("count"), new SumProperty("amount")))
+            );
+        //group by other.id, remove other ids (src and dst), aggregate by count and sum(amount)
+
+
+
         return null;
     }
 }
