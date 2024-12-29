479fed423843f92061eeb0857abc534b3bcad4b7
cubexy <54584804+cubexy@users.noreply.github.com>
2024-12-29 21:38:10 +0100

    added cr8 handling code

--- src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/complex/read8/ComplexRead8GradoopOperator.java
+++ src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/complex/read8/ComplexRead8GradoopOperator.java
@@ -1,34 +1,37 @@
 package org.ldbcouncil.finbench.impls.gradoop.queries.complex.read8;
 
 import java.util.Date;
+import java.util.List;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.gradoop.flink.model.api.operators.UnaryBaseGraphToValueOperator;
 import org.gradoop.temporal.model.impl.TemporalGraph;
 import org.ldbcouncil.finbench.driver.truncation.TruncationOrder;
 import org.ldbcouncil.finbench.driver.workloads.transaction.queries.ComplexRead8;
+import org.ldbcouncil.finbench.driver.workloads.transaction.queries.ComplexRead8Result;
 
 class ComplexRead8GradoopOperator implements
-    UnaryBaseGraphToValueOperator<TemporalGraph, DataSet<Tuple3<Long, Float, Integer>>> {
+    UnaryBaseGraphToValueOperator<TemporalGraph, List<ComplexRead8Result>> {
 
-    private final Long id;
-    private final Date startTime;
-    private final Date endTime;
-    private final Integer truncationLimit;
-    private final Float threshold;
-    private final TruncationOrder truncationOrder;
+    private final long id;
+    private final long startTime;
+    private final long endTime;
+    private final int truncationLimit;
+    private final boolean isTruncationOrderAscending;
+    private final double threshold;
 
     public ComplexRead8GradoopOperator(ComplexRead8 cr8) {
         this.id = cr8.getId();
-        this.startTime = cr8.getStartTime();
-        this.endTime = cr8.getEndTime();
+        this.startTime = cr8.getStartTime().getTime();
+        this.endTime = cr8.getEndTime().getTime();
         this.truncationLimit = cr8.getTruncationLimit();
-        this.truncationOrder = cr8.getTruncationOrder();
+        final TruncationOrder truncationOrder = cr8.getTruncationOrder();
+        this.isTruncationOrderAscending = truncationOrder == TruncationOrder.TIMESTAMP_ASCENDING;
         this.threshold = cr8.getThreshold();
     }
 
     @Override
-    public DataSet<Tuple3<Long, Float, Integer>> execute(TemporalGraph temporalGraph) {
+    public List<ComplexRead8Result> execute(TemporalGraph temporalGraph) {
         return null;
     }
 }
--- src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/complex/read8/ComplexRead8Handler.java
+++ src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/complex/read8/ComplexRead8Handler.java
@@ -18,14 +18,7 @@ public class ComplexRead8Handler implements OperationHandler<ComplexRead8, Grado
     public void executeOperation(ComplexRead8 cr8, GradoopFinbenchBaseGraphState connectionState,
                                  ResultReporter resultReporter) throws DbException {
         GradoopImpl.logger.info(cr8.toString());
-        DataSet<Tuple3<Long, Float, Integer>> complexRead8Result = new ComplexRead8GradoopOperator(cr8).execute(connectionState.getGraph());
-        List<ComplexRead8Result> complexRead8Results = new ArrayList<>();
-        try {
-            complexRead8Result.collect().forEach(
-                tuple -> complexRead8Results.add(new ComplexRead8Result(tuple.f0, tuple.f1, tuple.f2)));
-        } catch (Exception e) {
-            throw new DbException("Error while collecting results for complex read 8: " + e);
-        }
+        List<ComplexRead8Result> complexRead8Results = new ComplexRead8GradoopOperator(cr8).execute(connectionState.getGraph());
         resultReporter.report(complexRead8Results.size(), complexRead8Results, cr8);
     }
 }
