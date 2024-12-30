3a88259e2681715a0ae6b7205412c18c4024d481
cubexy <54584804+cubexy@users.noreply.github.com>
2024-12-30 17:08:39 +0100

    finished cr9

--- src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/complex/read9/ComplexRead9GradoopOperator.java
+++ src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/complex/read9/ComplexRead9GradoopOperator.java
@@ -1,17 +1,26 @@
 package org.ldbcouncil.finbench.impls.gradoop.queries.complex.read9;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.DataSet;
-import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.operators.ReduceOperator;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.gradoop.common.model.impl.id.GradoopId;
+import org.gradoop.common.model.impl.pojo.EPGMEdge;
 import org.gradoop.flink.model.api.operators.UnaryBaseGraphToValueOperator;
 import org.gradoop.flink.model.impl.functions.epgm.LabelIsIn;
 import org.gradoop.flink.model.impl.layouts.transactional.tuples.GraphTransaction;
 import org.gradoop.temporal.model.impl.TemporalGraph;
 import org.ldbcouncil.finbench.driver.truncation.TruncationOrder;
 import org.ldbcouncil.finbench.driver.workloads.transaction.queries.ComplexRead9;
-import org.ldbcouncil.finbench.driver.workloads.transaction.queries.ComplexRead9;
 import org.ldbcouncil.finbench.driver.workloads.transaction.queries.ComplexRead9Result;
+import org.ldbcouncil.finbench.impls.gradoop.CommonUtils;
 
 class ComplexRead9GradoopOperator implements
     UnaryBaseGraphToValueOperator<TemporalGraph, List<ComplexRead9Result>> {
@@ -35,28 +44,97 @@ class ComplexRead9GradoopOperator implements
 
     @Override
     public List<ComplexRead9Result> execute(TemporalGraph temporalGraph) {
-        final double thresholdSerializable = this.threshold;
 
         TemporalGraph windowedGraph = temporalGraph
             .subgraph(new LabelIsIn<>("Account", "Loan"), new LabelIsIn<>("transfer", "repay", "deposit"))
             .fromTo(this.startTime, this.endTime);
 
-        try {
-            List<GraphTransaction> t = windowedGraph.query("MATCH (loan:Loan)-[edge1:deposit]->(mid:Account)-[edge2:repay]->(loan)," +
-                " (up:Account)-[edge3:transfer]->(mid)-[edge4:transfer]->(down:Account)" +
-                " WHERE mid.id = " + this.id + "L AND edge1.amount > " + this.threshold +
-                " AND edge2.amount > " + this.threshold +
-                " AND edge3.amount > " + this.threshold +
-                " AND edge4.amount > " + this.threshold)
-                .toGraphCollection()
-                .getGraphTransactions()
-                .collect();
+//        windowedGraph.query("MATCH (loan1:Loan)-[edge1:deposit]->(mid:Account)-[edge2:repay]->(loan2:Loan)," +
+//                " (up:Account)-[edge3:transfer]->(mid)-[edge4:transfer]->(down:Account)" +
+//                " WHERE mid.id = " + this.id + "L" +
+//                " AND edge1.amount > " + this.threshold + " AND edge2.amount > " + this.threshold +
+//                " AND edge3.amount > " + this.threshold + " AND edge4.amount > " + this.threshold)
+//            .toGraphCollection()
+//            .getGraphTransactions();
 
-            t = t;
+        DataSet<GraphTransaction> edge1gt = windowedGraph.query("MATCH (loan1:Loan)-[edge1:deposit]->(mid:Account)" +
+                " WHERE mid.id = " + this.id + "L" +
+                " AND edge1.amount > " + this.threshold)
+            .toGraphCollection()
+            .getGraphTransactions();
+
+        DataSet<GraphTransaction> edge2gt = windowedGraph.query("MATCH (mid:Account)-[edge2:repay]->(loan2:Loan)" +
+                " WHERE mid.id = " + this.id + "L" +
+                " AND edge2.amount > " + this.threshold)
+            .toGraphCollection()
+            .getGraphTransactions();
+
+        DataSet<GraphTransaction> edge3gt = windowedGraph.query("MATCH (up:Account)-[edge3:transfer]->(mid:Account)" +
+                " WHERE mid.id = " + this.id + "L" +
+                " AND edge3.amount > " + this.threshold)
+            .toGraphCollection()
+            .getGraphTransactions();
+
+        DataSet<GraphTransaction> edge4gt = windowedGraph.query("MATCH (mid:Account)-[edge4:transfer]->(down:Account)" +
+                " WHERE mid.id = " + this.id + "L" +
+                " AND edge4.amount > " + this.threshold)
+            .toGraphCollection()
+            .getGraphTransactions();
+
+        DataSet<Tuple4<Double, Double, Double, Double>> result = edge1gt.union(edge2gt).union(edge3gt).union(edge4gt).map(
+            new MapFunction<GraphTransaction, Tuple4<Double, Double, Double, Double>>() {
+                @Override
+                public Tuple4<Double, Double, Double, Double> map(GraphTransaction graphTransaction) throws Exception {
+                    Set<EPGMEdge> edges = graphTransaction.getEdges();
+
+                    double edge1Amount = 0;
+                    double edge2Amount = 0;
+                    double edge3Amount = 0;
+                    double edge4Amount = 0;
+
+                    for (EPGMEdge edge : edges) {
+                        double edgeAmount = edge.getPropertyValue("amount").getDouble();
+                        if (edge.getLabel().equals("deposit")) {
+                            edge1Amount = edgeAmount;
+                            continue;
+                        }
+                        if (edge.getLabel().equals("repay")) {
+                            edge2Amount = edgeAmount;
+                            continue;
+                        }
+                        // edge type 3 and 4 are transfers - we can find out which one it is by looking at the source and target
+                        Map<String, GradoopId> m = CommonUtils.getVariableMapping(graphTransaction);
+                        GradoopId midGradoopId = m.get("mid");
+                        if (edge.getTargetId().equals(midGradoopId)) {
+                            edge3Amount = edgeAmount;
+                            continue;
+                        }
+                        edge4Amount = edgeAmount;
+                    }
+                    return new Tuple4<>(edge1Amount, edge2Amount, edge3Amount, edge4Amount);
+                }
+            }
+        )
+            .reduce(new ReduceFunction<Tuple4<Double, Double, Double, Double>>() {
+                @Override
+                public Tuple4<Double, Double, Double, Double> reduce(Tuple4<Double, Double, Double, Double> t1, Tuple4<Double, Double, Double, Double> t2) throws Exception {
+                    return new Tuple4<>(t1.f0 + t2.f0, t1.f1 + t2.f1, t1.f2 + t2.f2, t1.f3 + t2.f3);
+                }
+            });
+
+        Tuple4<Double, Double, Double, Double> data = null;
+
+        try {
+            data = result.collect().get(0);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
-        return null;
+        double ratioRepay = data.f1.equals(0.0) ? data.f0 / data.f1 : -1.0;
+        double ratioDeposit = data.f3.equals(0.0) ? data.f0 / data.f3 : -1.0;
+        double ratioTransfer = data.f3.equals(0.0) ? data.f2 / data.f3 : -1.0;
+
+        return Collections.singletonList(new ComplexRead9Result((float) ratioRepay, (float) ratioDeposit,
+            (float) ratioTransfer));
     }
 }
