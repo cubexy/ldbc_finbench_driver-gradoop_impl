a0e3a4696b6a79d681e0fd6f546c1c6a4b5f40f2
cubexy <54584804+cubexy@users.noreply.github.com>
2025-01-03 23:39:36 +0100

    modified sr1 to use getVertices

--- src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/simple/read1/SimpleRead1GradoopOperator.java
+++ src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/simple/read1/SimpleRead1GradoopOperator.java
@@ -6,12 +6,15 @@ import java.util.List;
 import java.util.Map;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.operators.MapOperator;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.gradoop.common.model.impl.id.GradoopId;
 import org.gradoop.common.model.impl.pojo.EPGMVertex;
 import org.gradoop.flink.model.api.operators.UnaryBaseGraphToValueOperator;
 import org.gradoop.flink.model.impl.layouts.transactional.tuples.GraphTransaction;
+import org.gradoop.flink.model.impl.operators.combination.ReduceCombination;
 import org.gradoop.temporal.model.impl.TemporalGraph;
+import org.gradoop.temporal.model.impl.pojo.TemporalVertex;
 import org.ldbcouncil.finbench.driver.workloads.transaction.queries.SimpleRead1;
 import org.ldbcouncil.finbench.driver.workloads.transaction.queries.SimpleRead1Result;
 import org.ldbcouncil.finbench.impls.gradoop.CommonUtils;
@@ -26,28 +29,22 @@ public class SimpleRead1GradoopOperator implements
 
     @Override
     public List<SimpleRead1Result> execute(TemporalGraph temporalGraph) {
-        DataSet<GraphTransaction> accounts = temporalGraph.query("MATCH (a:Account) WHERE a.id = " + id + "L")
-            .toGraphCollection()
-            .getGraphTransactions();
-
-        DataSet<Tuple3<Date, Boolean, String>> dataSetResult = accounts
-            .map(new MapFunction<GraphTransaction, Tuple3<Date, Boolean, String>>() {
+        DataSet<Tuple3<Date, Boolean, String>>
+            dataSetResult = temporalGraph.query("MATCH (a:Account) WHERE a.id = " + id + "L")
+            .reduce(new ReduceCombination<>())
+            .getVertices()
+            .map(new MapFunction<TemporalVertex, Tuple3<Date, Boolean, String>>() {
                 @Override
-                public Tuple3<Date, Boolean, String> map(GraphTransaction graphTransaction) throws Exception {
-                    Map<String, GradoopId> m = CommonUtils.getVariableMapping(graphTransaction);
-
-                    GradoopId accountId = m.get("a");
-
-                    EPGMVertex account = graphTransaction.getVertexById(accountId);
-
+                public Tuple3<Date, Boolean, String> map(TemporalVertex temporalVertex) throws Exception {
                     Date createTime =
-                        CommonUtils.parseUnixTimeString(account.getPropertyValue("createTime").toString());
-                    Boolean isBlocked = account.getPropertyValue("isBlocked").getBoolean();
-                    String type = account.getPropertyValue("type").getString();
+                        CommonUtils.parseUnixTimeString(temporalVertex.getPropertyValue("createTime").toString());
+                    Boolean isBlocked = temporalVertex.getPropertyValue("isBlocked").getBoolean();
+                    String type = temporalVertex.getPropertyValue("type").getString();
 
                     return new Tuple3<>(createTime, isBlocked, type);
                 }
             });
+
         List<SimpleRead1Result> simpleRead1Results = new ArrayList<>();
         try {
             dataSetResult.collect().forEach(
