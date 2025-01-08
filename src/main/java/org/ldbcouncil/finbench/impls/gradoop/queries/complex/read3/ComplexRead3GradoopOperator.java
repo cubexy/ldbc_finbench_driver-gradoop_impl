2fe5c729e9d1eb3607d27e58b784b130c51c1c72
cubexy <54584804+cubexy@users.noreply.github.com>
2025-01-08 21:30:12 +0100

    fixed wrong filter

--- src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/complex/read3/ComplexRead3GradoopOperator.java
+++ src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/complex/read3/ComplexRead3GradoopOperator.java
@@ -71,7 +71,7 @@ class ComplexRead3GradoopOperator implements UnaryBaseGraphToValueOperator<Tempo
         try {
             dst = SSSPGraph.getVertices()
                 .filter(ver -> ver.hasProperty("id"))
-                .filter(ver -> (long) ver.getPropertyValue("id").getDouble() == serializableId2).collect();
+                .filter(ver -> (long) ver.getPropertyValue("distance").getDouble() == serializableId2).collect();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
