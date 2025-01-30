6f95b3b222b9fa43e647b43b588c3327f6401630
cubexy <54584804+cubexy@users.noreply.github.com>
2025-01-30 18:34:38 +0100

    fixed cr2

--- src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/complex/read2/ComplexRead2GradoopOperator.java
+++ src/main/java/org/ldbcouncil/finbench/impls/gradoop/queries/complex/read2/ComplexRead2GradoopOperator.java
@@ -3,26 +3,26 @@ package org.ldbcouncil.finbench.impls.gradoop.queries.complex.read2;
 import static org.ldbcouncil.finbench.impls.gradoop.CommonUtils.roundToDecimalPlaces;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.operators.Order;
 import org.apache.flink.api.java.DataSet;
-import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.gradoop.common.model.impl.id.GradoopId;
 import org.gradoop.common.model.impl.pojo.EPGMVertex;
 import org.gradoop.flink.model.api.operators.UnaryBaseGraphToValueOperator;
 import org.gradoop.flink.model.impl.epgm.GraphCollection;
-import org.gradoop.flink.model.impl.epgm.LogicalGraph;
 import org.gradoop.flink.model.impl.functions.epgm.LabelIsIn;
-import org.gradoop.flink.model.impl.operators.aggregation.functions.sum.SumProperty;
+import org.gradoop.flink.model.impl.layouts.transactional.tuples.GraphTransaction;
 import org.gradoop.flink.model.impl.operators.combination.ReduceCombination;
-import org.gradoop.flink.model.impl.operators.keyedgrouping.GroupingKeys;
-import org.gradoop.flink.model.impl.operators.keyedgrouping.KeyedGrouping;
 import org.gradoop.temporal.model.impl.TemporalGraph;
 import org.ldbcouncil.finbench.driver.truncation.TruncationOrder;
 import org.ldbcouncil.finbench.driver.workloads.transaction.queries.ComplexRead2;
 import org.ldbcouncil.finbench.driver.workloads.transaction.queries.ComplexRead2Result;
+import org.ldbcouncil.finbench.impls.gradoop.CommonUtils;
 
 class ComplexRead2GradoopOperator implements
     UnaryBaseGraphToValueOperator<TemporalGraph, List<ComplexRead2Result>> {
@@ -80,32 +80,34 @@ class ComplexRead2GradoopOperator implements
                     "L AND e2.val_from.before(t1.val_from) AND t1.val_from.before(t2.val_from)")
             .toGraphCollection();
 
-        LogicalGraph gcUnion = gtxLength1.union(gtxLength2).union(gtxLength3).reduce(new ReduceCombination<>())
-            .query("MATCH (other:Account)<-[e3:deposit]-(loan:Loan)")
-            .reduce(new ReduceCombination<>())
-            .transformVertices((currentVertex, transformedVertex) -> {
-                if (!currentVertex.getLabel().equals("Loan")) {
-                    currentVertex.removeProperty("id");
-                }
-                return currentVertex;
-            })
-            .callForGraph(
-                new KeyedGrouping<>(Arrays.asList(GroupingKeys.label(), GroupingKeys.property("id")),
-                    Arrays.asList(new SumProperty("loanAmount"), new SumProperty("balance")), null,
-                    null)
-            );
+        DataSet<GraphTransaction> gtUnion =
+            gtxLength1.union(gtxLength2).union(gtxLength3).reduce(new ReduceCombination<>())
+                .query("MATCH (other:Account)<-[e3:deposit]-(loan:Loan)") // get last part of unioned query
+                .getGraphTransactions();
 
-        DataSet<Tuple3<Long, Double, Double>>
-            edgeMap = gcUnion.getVertices().filter(new LabelIsIn<>("Loan"))
-            .map(new MapFunction<EPGMVertex, Tuple3<Long, Double, Double>>() {
+        DataSet<Tuple4<Long, Double, Double, Long>>
+            edgeMap = gtUnion.map(new MapFunction<GraphTransaction, Tuple4<Long, Double, Double, Long>>() {
                 @Override
-                public Tuple3<Long, Double, Double> map(EPGMVertex src) {
-                    long otherID = src.getPropertyValue("id").getLong();
-                    double loanAmount = src.getPropertyValue("sum_loanAmount").getDouble();
-                    double loanBalance = src.getPropertyValue("sum_balance").getDouble();
-                    return new Tuple3<>(otherID, loanAmount, loanBalance);
+                public Tuple4<Long, Double, Double, Long> map(GraphTransaction graphTransaction) {
+                    Map<String, GradoopId> m = CommonUtils.getVariableMapping(graphTransaction);
+                    EPGMVertex other = graphTransaction.getVertexById(m.get("other"));
+                    EPGMVertex loan = graphTransaction.getVertexById(m.get("loan"));
+
+                    long otherID = other.getPropertyValue("id").getLong();
+                    double loanAmount = loan.getPropertyValue("loanAmount").getDouble();
+                    double loanBalance = loan.getPropertyValue("balance").getDouble();
+                    long loanID = loan.getPropertyValue("id").getLong();
+                    return new Tuple4<>(otherID, loanAmount, loanBalance, loanID);
                 }
 
+            }).distinct(0, 3)
+            .groupBy(0)
+            .reduce(new ReduceFunction<Tuple4<Long, Double, Double, Long>>() {
+                @Override
+                public Tuple4<Long, Double, Double, Long> reduce(Tuple4<Long, Double, Double, Long> t1,
+                                                                 Tuple4<Long, Double, Double, Long> t2) {
+                    return new Tuple4<>(t1.f0, t1.f1 + t2.f1, t1.f2 + t2.f2, 0L);
+                }
             });
 
         if (this.useFlinkSort) {
@@ -116,7 +118,7 @@ class ComplexRead2GradoopOperator implements
                 .sortPartition(0, Order.ASCENDING);
         }
 
-        List<Tuple3<Long, Double, Double>> edges;
+        List<Tuple4<Long, Double, Double, Long>> edges;
         try {
             edges = edgeMap.collect();
         } catch (Exception e) {
@@ -125,13 +127,13 @@ class ComplexRead2GradoopOperator implements
 
         if (!this.useFlinkSort) {
             edges.sort(Comparator
-                .comparing((Tuple3<Long, Double, Double> t) -> t.f1, Comparator.reverseOrder())
+                .comparing((Tuple4<Long, Double, Double, Long> t) -> t.f1, Comparator.reverseOrder())
                 .thenComparing(t -> t.f0));
         }
 
         List<ComplexRead2Result> complexRead2Results = new ArrayList<>();
 
-        for (Tuple3<Long, Double, Double> edge : edges) {
+        for (Tuple4<Long, Double, Double, Long> edge : edges) {
             complexRead2Results.add(
                 new ComplexRead2Result(edge.f0, roundToDecimalPlaces(edge.f1, 3), roundToDecimalPlaces(edge.f2, 3)));
         }
